2523fee70565ca2f0b1d84d2fbec70fc0d576dfc
Tobias Helsing <tobias.helsing@slush.org>
2019-11-27 14:33:08 +0200

    Added browse excuse

--- src/app/browse-view/browse-view.module.ts
+++ src/app/browse-view/browse-view.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { BrowseViewRoutingModule } from './browse-view-routing.module';
-import { BrowseViewComponent } from './browse-view.component';
-
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { LayoutModule } from "../components/layout/layout.module";
+import { SingleViewNavModule } from "../components/single-view-nav/single-view-nav.module";
+import { BrowseViewRoutingModule } from "./browse-view-routing.module";
+import { BrowseViewComponent } from "./browse-view.component";
 
 @NgModule({
   declarations: [BrowseViewComponent],
   imports: [
     CommonModule,
+    LayoutModule,
+    SingleViewNavModule,
     BrowseViewRoutingModule
   ]
 })
-export class BrowseViewModule { }
+export class BrowseViewModule {}
